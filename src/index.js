1eaac0b1b842d6c6ba5c52795ae9da63a997db73
Musab Khan <godxxillaa@gmail.com>
2025-01-13 08:33:31 +0400

    Added utils files and wrote some code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ import  connectDB  from "./db/index.js";
 
 const app = express()
 connectDB()
+.then(
+    () => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+            
+        })
+    }
+)
+.catch((err) => {
+    console.log(`Mongo DB Connection Faild: ${err} `)
+})
 
 
 
